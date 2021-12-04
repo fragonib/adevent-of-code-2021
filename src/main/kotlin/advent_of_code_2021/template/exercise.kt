345fbed3b3494b5005ee2a601e7bb9b231c605b0
Francisco González Ibáñez <fragonib@gmail.com>
2021-12-04 11:50:27 +0100

    Extract day template

---
+++ src/main/kotlin/advent_of_code_2021/template/exercise.kt
@@ -0,0 +1,13 @@
+package advent_of_code_2021.template
+
+import advent_of_code_2021.shared.parseInput
+
+
+fun main() {
+    println(resolve("dayX/partX/input.txt"))
+}
+
+internal fun resolve(inputSource: String): Int {
+    val depths = parseInput(inputSource)
+    TODO()
+}
---
+++ src/main/kotlin/advent_of_code_2021/template/test.kt
@@ -0,0 +1,8 @@
+package advent_of_code_2021.template
+
+import org.assertj.core.api.Assertions.assertThat
+
+fun main() {
+    val result = resolve("dayX/partX/test_input.txt")
+    assertThat(result).isEqualTo(7)
+}

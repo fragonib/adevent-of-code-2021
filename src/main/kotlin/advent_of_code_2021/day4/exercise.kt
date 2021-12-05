24f3b09ea4df3a86c5aca19e3f4bf17f6019a529
Francisco González Ibáñez <fragonib@gmail.com>
2021-12-05 12:19:47 +0100

    Day 4 Part 1

---
+++ src/main/kotlin/advent_of_code_2021/day4/exercise.kt
@@ -0,0 +1,13 @@
+package advent_of_code_2021.day4
+
+import advent_of_code_2021.shared.parseInput
+
+
+fun main() {
+    println(resolve("day4/part1/input.txt"))
+}
+
+internal fun resolve(inputSource: String): Int {
+    val depths = parseInput(inputSource)
+    TODO()
+}
---
+++ src/main/kotlin/advent_of_code_2021/day4/test.kt
@@ -0,0 +1,8 @@
+package advent_of_code_2021.day4
+
+import org.assertj.core.api.Assertions.assertThat
+
+fun main() {
+    val result = resolve("day4/part1/test_input.txt")
+    assertThat(result).isEqualTo(4512)
+}
